7c6e7dce2f573cf36deb071c93ccf5d632cc12d0
subinnie127 <romysynyster5@gmail.com>
2023-06-30 17:48:52 -0400

    validacion contacto

--- TiendaTaller/core/views.py
+++ TiendaTaller/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from .models import Vehiculo, Categoria
-from .forms import VehiculoForm
+from .forms import VehiculoForm, ContactForm
 
 # Create your views here.
 
@@ -68,3 +68,15 @@ def contacto(request):
     return render(request, "core/contacto.html")
 def nosotros(request):
     return render(request, "core/nosotros.html")
+def contacto(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Procesa los datos del formulario
+            # Por ejemplo, puedes guardar los datos en la base de datos o enviar un correo electrónico
+            # ...
+            return render(request, 'success.html')  # Renderiza una página de éxito después de enviar el formulario
+    else:
+        form = ContactForm()
+    
+    return render(request, 'core/contacto.html', {'form': form})
